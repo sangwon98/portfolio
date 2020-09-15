2f1b6cb7e2a3397ee2fe3f6ffabf819a23f8ebf9
sangwon98 <llfkstep9811@naver.com>
2020-09-15 18:27:09 +0900

    js navbar menu click

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 "use strict";
-
 //navbar창 스크롤 이벤트
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
+  /*
+  console.log(window.scrollY);
+  console.log(`Hegiht: ${navbarHeight}`);
+*/
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+//클릭하면 해당하는 곳으로 넘어가기
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
